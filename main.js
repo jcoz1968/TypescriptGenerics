3293a5b596d987d4b0d1a79ab9a110abe65cd5d9
Jeffery Cosby <jcoz1968@gmail.com>
2019-11-26 20:31:11 -0600

    added missed push

---
+++ main.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var conferenceRoom_1 = require("./models/conferenceRoom");
+function getBigRooms(rooms, minSize) {
+    var bigRooms = [];
+    rooms.forEach(function (r) {
+        if (r.capacity > minSize) {
+            bigRooms.push(r);
+        }
+    });
+    return bigRooms;
+}
+var getLargeRooms;
+getLargeRooms = getBigRooms;
+var largeRooms = getLargeRooms(conferenceRoom_1.conferenceRoomData, 30);
+console.log(largeRooms);
+// let bigRooms: Array<PartyTent> = getBigRooms<PartyTent>(partyTentData, 20);
+// console.log(bigRooms);
+// let junkDrawer: any[] = ['cool string', 452, true];
+// let companies: Array<string> = ['Microsoft', 'Google', 'Amazon'];
+// let primeNums: Array<number> = [1, 15];
+// let lastValue = primeNums.pop();
+function shortenArray(data, amountToShorten) {
+    return data.splice(amountToShorten, data.length);
+}
+var shrinkArray;
+shrinkArray = shortenArray;
+console.log(shrinkArray);
+// let stringArray: string[] = ['Visual Basic', 'C#', 'T-SQL', 'JavaScript'];
+// let fewerLanguages: Array<string> = shortenArray<string>(stringArray, 2);
+// console.log(fewerLanguages)
--- main.ts
+++ main.ts
@@ -1,7 +1,38 @@
-let junkDrawer: any[] = ['cool string', 452, true];
+import { PartyTent , partyTentData} from './models/partyTent';
+import { ConferenceRoom, conferenceRoomData } from './models/conferenceRoom';
+import { MeetingResource } from './models/meetingResource';
+import { Building, buildingData } from './models/building';
 
-let companies: Array<string> = ['Microsoft', 'Google', 'Amazon'];
+function getBigRooms<T extends MeetingResource>(rooms: Array<T>, minSize: number): Array <T> {
+  let bigRooms: Array<T> = [];
+  rooms.forEach(r => { 
+    if (r.capacity > minSize) {
+      bigRooms.push(r);
+    }
+  });
+  return bigRooms;
+}
 
-let primeNums: Array<number> = [1, 15];
+let getLargeRooms: <T extends MeetingResource>(rooms: Array<T>, minSize: number) => Array<T>;
+getLargeRooms = getBigRooms;
+let largeRooms = getLargeRooms(conferenceRoomData, 30);
+console.log(largeRooms);
 
-let lastValue = primeNums.pop();
+// let bigRooms: Array<PartyTent> = getBigRooms<PartyTent>(partyTentData, 20);
+// console.log(bigRooms);
+
+// let junkDrawer: any[] = ['cool string', 452, true];
+// let companies: Array<string> = ['Microsoft', 'Google', 'Amazon'];
+// let primeNums: Array<number> = [1, 15];
+// let lastValue = primeNums.pop();
+
+function shortenArray<T>(data: Array<T>, amountToShorten: number): Array<T> {
+  return data.splice(amountToShorten, data.length);
+}
+let shrinkArray: <U>(original: Array<U>, units: number) => Array<U>;
+shrinkArray = shortenArray;
+console.log(shrinkArray);
+// let stringArray: string[] = ['Visual Basic', 'C#', 'T-SQL', 'JavaScript'];
+// let fewerLanguages: Array<string> = shortenArray<string>(stringArray, 2);
+// console.log(fewerLanguages)
+
