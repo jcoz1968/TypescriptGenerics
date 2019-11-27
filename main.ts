317e1f39e62d4ae73238e1ec403dcc54d6115eec
Jeffery Cosby <jcoz1968@gmail.com>
2019-11-26 19:57:28 -0600

    generic arrays

---
+++ main.ts
@@ -0,0 +1,7 @@
+let junkDrawer: any[] = ['cool string', 452, true];
+
+let companies: Array<string> = ['Microsoft', 'Google', 'Amazon'];
+
+let primeNums: Array<number> = [1, 15];
+
+let lastValue = primeNums.pop();
