76673556a8a4d05c9e17621b0bf0c7e1b9ec4c7b
Jeffery Cosby <jcoz1968@gmail.com>
2019-11-26 20:52:10 -0600

    applying constraints to a generic class

---
+++ models/reservation.ts
@@ -0,0 +1,18 @@
+import { ConferenceRoom, conferenceRoomData } from './conferenceRoom';
+import { MeetingResource } from './meetingResource';
+
+class Reservation<T extends MeetingResource> {
+  reservationDate: Date;
+  organizerName: string;
+  resource: T;
+
+  requestResource(requestedResource: T, requester: string) {
+    this.resource = requestedResource;
+    this.organizerName = requester;
+    console.log(`${requester} requested a reservation ${requestedResource.name}`);
+
+  }
+}
+
+let teamMeeting = new Reservation<ConferenceRoom>();
+teamMeeting.requestResource(conferenceRoomData[0], 'Coz');
